6e39631fae824c19d75481d06ea12cbe5e34300c
백민석 <msbaek97@naver.com>
2022-08-11 14:17:22 +0900

    '2022-08-11'

---
+++ portfolio.js
@@ -0,0 +1,74 @@
+/* HEADER */
+window.onload = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var header = document.getElementById('header');
+
+    if(document.documentElement.scrollTop > 70) {
+        if(!header.classList.contains('navbar-fixed')) {
+            header.classList.add('navbar-fixed');
+            document.getElementsByTagName('body')[0].style.marginTop = '70px';
+            header.style.display = 'none';
+            setTimeout(function(){
+                header.style.display = 'block';
+            }, 40);
+        }
+    } else {
+        if(header.classList.contains('navbar-fixed')) {
+            header.classList.remove('navbar-fixed');
+            document.getElementsByTagName('body')[0].style.marginTop = '0';
+        }
+    }
+}
+
+function menuToggle() {
+    document.getElementById('menu').classList.toggle('show');
+}
+
+document.getElementById('toggleBtn').addEventListener('click', menuToggle);
+
+
+
+/* CAREER */
+var items = document.querySelectorAll("li");
+
+function isItemInView(item){
+  var rect = item.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isItemInView(items[i])) {
+        items[i].classList.add("show");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+
+/* ANCHOR */
+function moveTo(id) {
+  if(id == 'brand'){
+    window.scrollTo(0, 0);
+  } else {
+    window.scrollTo(0, document.getElementById(id).offsetTop - 70);
+  }
+  document.getElementById('menu').classList.remove('show');
+}
+
+document.getElementById('navbarBrand').addEventListener('click', moveTo.bind(null,'brand'));
+document.getElementById('navbarAbout').addEventListener('click', moveTo.bind(null,'about'));
+document.getElementById('navbarProjects').addEventListener('click', moveTo.bind(null,'Projects'));
+document.getElementById('navbarCareer').addEventListener('click', moveTo.bind(null,'Career'));
+document.getElementById('navbarReview').addEventListener('click', moveTo.bind(null,'review'));
