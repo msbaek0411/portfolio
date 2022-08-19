07c57ba27e267f61b9e4229c1ce005b33faa50b8
백민석 <msbaek97@naver.com>
2022-08-19 14:18:02 +0900

    '2022-08-19'

--- test/test.js
+++ test/test.js
@@ -1,31 +1,6 @@
-new Swiper('.swiper1', {
-	pagination : { // 페이징 설정
-		el : '.swiper-pagination',
-		clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
-	},
-	navigation : { // 네비게이션 설정
-		nextEl : '.swiper-button-next', // 다음 버튼 클래스명
-		prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
-	},
-});
-
-new Swiper('.swiper2', {
-	pagination : { // 페이징 설정
-		el : '.swiper-pagination',
-		clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
-	},
-	navigation : { // 네비게이션 설정
-		nextEl : '.swiper-button-next', // 다음 버튼 클래스명
-		prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
-	},
-});
-new Swiper('.swiper3', {
-	pagination : { // 페이징 설정
-		el : '.swiper-pagination',
-		clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
-	},
-	navigation : { // 네비게이션 설정
-		nextEl : '.swiper-button-next', // 다음 버튼 클래스명
-		prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
-	},
-});
+$(function() {
+	$('a[href*=#]').on('click', function(e) {
+	  e.preventDefault();
+	  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+	});
+  });
